55041ca70aa6ed6c47d4f7e278c9f3567a843223
RahulGangi <rockergang194@gmail.com>
2026-07-28 15:46:22 +0530

    Add files via upload

---
+++ relation.java
@@ -0,0 +1,13 @@
+class RelationalOperator
+{
+public static void main(String[]args)
+{
+int a=10;int b=3;int c=5;
+System.out.println("a>b:"+(a>b));
+System.out.println("a<b:"+(a>b));
+System.out.println("a>=b:"+(a>=b));
+System.out.println("a<=b:"+(a<=b));
+System.out.println("a==b:"+(a==b));
+System.out.println("a!=b:"+(a!=b));
+}
+}
